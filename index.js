bed53705f27c6af323619f2dcfb28d09daa1389a
ralph kemp <ralphk3mp@gmail.com>
2018-09-04 13:17:46 +0100

    added basic animation to elems

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('hey!')
+const tl = new TimelineLite();
+
+tl.to('#steel-logo', 0.5, {opacity: 1})
+  .to('#watch', 0.5, {opacity: 1}, "-=0.3")
+  .to('#cta', 1, {opacity: 1, delay: 1});
