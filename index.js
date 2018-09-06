1815cd741755e43070159f911f410f4161e7434b
ralph kemp <ralphk3mp@gmail.com>
2018-09-06 12:34:44 +0100

    moved js into ecoStart function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const tl = new TimelineLite();
+function ecoStart() {
+  const tl = new TimelineLite();
 
-tl.to('#steel-logo', 0.5, {opacity: 1})
-  .to('#watch', 0.5, {opacity: 1}, "-=0.3")
-  .to('#cta', 1, {opacity: 1, delay: 1});
+  tl.to('#steel-logo', 0.5, { opacity: 1 })
+    .to('#watch', 0.5, { opacity: 1 }, '-=0.3')
+    .to('#cta', 1, { opacity: 1, delay: 1 });
+}
